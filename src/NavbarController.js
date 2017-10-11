41eb6b9d6435da31ee5e9d96525b36148ad5ac05
labroche <corentin.labroche@inist.fr>
2017-10-11 04:26:34 +0200

    fix mp3 navbar

--- src/NavbarController.js
+++ src/NavbarController.js
@@ -1,10 +1,10 @@
 function NavbarController ($scope, ConfigService, $interval) {
-  const audio = new Audio('../resources/musics/theme.mp3');
+  // const audio = new Audio('https://raw.githubusercontent.com/clabrocheOtherProjects/Sanatorium/master/resources/musics/theme.mp3');
   $scope.getCurrentPage = _ => ConfigService.get('currentStoryPage');
   $scope.music = _ => {
-    if (audio.paused) audio.play();
-    else audio.pause();
-    $scope.audioPlaying = !audio.paused;
+    // if (audio.paused) audio.play();
+    // else audio.pause();
+    // $scope.audioPlaying = !audio.paused;
   };
 }
 module.exports = NavbarController;
